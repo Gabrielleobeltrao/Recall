ae359551ae44634071189d98039cb4d3e9178c71
Gabriel Leo Beltrao <gabrielleoaus@gmail.com>
2024-06-02 16:49:55 -0400

    :heavy_plus_sign: add ReactRouterDom

--- src/App.js
+++
@@ -1,7 +0,0 @@
-function App() {
-  return (
-   <h1 className="text-base">Hello Word</h1>
-  );
-}
-
-export default App;
--- src/index.js
+++ src/index.js
@@ -1,11 +1,22 @@
-import React from 'react';
 import ReactDOM from 'react-dom/client';
-import App from './App';
+import { BrowserRouter, Routes, Route } from "react-router-dom";
 import './index.css';
+import LandingPage from './pages/LandingPage';
+import Login from './pages/Login';
+import Home from './pages/Home';
+
+export default function AppRouter() {
+  return (
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<LandingPage />}>
+          <Route path='login' element={<Login />} />
+          <Route path='home' element={<Home />} />
+        </Route>
+      </Routes>
+    </BrowserRouter>
+  );
+}
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
-);
+root.render(<AppRouter />);
