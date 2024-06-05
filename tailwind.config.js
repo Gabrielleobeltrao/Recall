9401b49ce0fbfcc401b707748547a127cd21ea7c
Gabriel Leo Beltrao <gabrielleoaus@gmail.com>
2024-06-04 21:46:57 -0400

    :lipstick: add color and font

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,18 @@
 module.exports = {
   content: ["./src/**/*.{html,js}"],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        'pink': '#DE61E8',
+        'pink-purple': '#C266F2',
+        'purple': '#9467DB',
+        'purple-blue': '#7566F2',
+        'blue': '#6177E8'
+      },
+      fontFamily: {
+        sans: ['BRCobane', 'sans-serif'],
+      },
+    },
   },
   plugins: [],
 }
