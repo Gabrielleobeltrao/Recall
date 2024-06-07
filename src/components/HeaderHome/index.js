f5787cbb5eb5c2795038e4ba2d8eebda01007cbb
Gabriel Leo Beltrao <gabrielleoaus@gmail.com>
2024-06-07 16:56:12 -0400

    :lipstick: adjusting padding

--- src/components/HeaderHome/index.js
+++ src/components/HeaderHome/index.js
@@ -2,7 +2,7 @@ import Logo from "../Logo"
 
 function HeaderHome() {
     return (
-        <div className="flex justify-between items-center py-5 px-8">
+        <div className="flex justify-between items-center py-5 px-7">
             <Logo />
             <div className="text-end">
                 <h2 className="text-sm">gabrielleoaus@gmail.com</h2>
--- src/components/HeaderLandingPage/index.js
+++ src/components/HeaderLandingPage/index.js
@@ -3,7 +3,7 @@ import { Link } from "react-router-dom"
 
 function HeaderLandingPage() {
     return (
-        <div className="flex justify-between items-center py-5 px-8">
+        <div className="flex justify-between items-center py-5 px-7">
             <Logo />
             <Link to="/login" className="flex justify-center items-center w-28 h-8 bg-gradient-to-r from-pink via-purple to-blue rounded-lg">
                 <h3 className="text-white text-base font-bold tracking-wide">Entrar</h3>
