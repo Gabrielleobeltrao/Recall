9852f867aae70fbc8e2ae8fd5f4af39c57ab3264
Gabriel Leo Beltrao <gabrielleoaus@gmail.com>
2024-06-12 16:53:41 -0400

    :lipstick: adjust login page position

--- src/pages/Login.js
+++ src/pages/Login.js
@@ -2,9 +2,9 @@ import LogoByOne from "../components/LogoByOne"
 
 function Login() {
     return (
-        <div className="xl:flex xl:w-full xl:justify-between">
+        <div className="xl:flex xl:w-full xl:justify-between xl:h-screen xl:items-center xl:py-16">
             <div className="xl:w-full">
-                <LogoByOne className="pt-24 pb-16"/>
+                <LogoByOne className="pt-24 pb-16 xl:pt-0"/>
                 <div className="flex flex-col gap-5 px-16">
                     <input 
                         className="bg-neutral-200 rounded-2xl w-full pl-3 placeholder:text-black placeholder:text-base py-1.5" 
@@ -28,7 +28,7 @@ function Login() {
                     >Entrar</button>
                 </div>
             </div>
-            <div className="px-10 pt-12">
+            <div className="px-10 pt-12 xl:pt-0 xl:h-full">
                 <div className="w-full h-1 bg-gradient-to-r from-pink via-purple to-blue xl:h-full xl:w-1"/>
             </div>
             <div>
