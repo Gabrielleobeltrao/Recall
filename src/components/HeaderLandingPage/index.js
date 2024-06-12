749599d0ec1e7e3ec730d33b3be554bf8f3f76f5
Gabriel Leo Beltrao <gabrielleoaus@gmail.com>
2024-06-11 20:47:48 -0400

    :lipstick: responsive landing page

--- src/components/HeaderLandingPage/index.js
+++ src/components/HeaderLandingPage/index.js
@@ -3,10 +3,10 @@ import { Link } from "react-router-dom"
 
 function HeaderLandingPage() {
     return (
-        <div className="flex justify-between items-center py-5 px-7 lg:min-h-20">
-            <Logo className="lg:h-12" />
-            <Link to="/login" className="flex justify-center items-center w-28 h-8 bg-gradient-to-r from-pink via-purple to-blue rounded-lg lg:w-32 lg:h-9">
-                <h3 className="text-white text-base font-bold tracking-wide lg:text-xl">Entrar</h3>
+        <div className="flex justify-between items-center py-5 px-7 xl:min-h-20">
+            <Logo className="xl:h-12" />
+            <Link to="/login" className="flex justify-center items-center w-28 h-8 bg-gradient-to-r from-pink via-purple to-blue rounded-lg xl:w-32 xl:h-9">
+                <h3 className="text-white text-base font-bold tracking-wide xl:text-xl">Entrar</h3>
             </Link>
         </div>
     )
